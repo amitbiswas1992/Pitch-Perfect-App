bf976626b76938351e5f7fe9c4bb616c566f8557
Maze Geek <mazegeek@mazes-mbp>
2018-04-24 02:16:12 -0400

    Initial Commit

---
+++ Pitch-Perfect-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch-Perfect-App
+//
+//  Created by Maze Geek on 4/24/18.
+//  Copyright © 2018 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pitch-Perfect-AppTests/Pitch_Perfect_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pitch_Perfect_AppTests.swift
+//  Pitch-Perfect-AppTests
+//
+//  Created by Maze Geek on 4/24/18.
+//  Copyright © 2018 Amit Biswas. All rights reserved.
+//
+
+import XCTest
+@testable import Pitch_Perfect_App
+
+class Pitch_Perfect_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
