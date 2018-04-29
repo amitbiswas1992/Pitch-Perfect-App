6c8998a8335ca3eee51139a1f14fbb8fe4faf25e
Amit Biswas <biswas1992@hotmail.com>
2018-04-29 17:08:39 -0400

    Playing back Audio andded tag at all the buttons

--- Pitch-Perfect-App/PlaySoundsViewController.swift
+++ Pitch-Perfect-App/PlaySoundsViewController.swift
@@ -11,6 +11,7 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
+    //Connecting Button with StoryBoard UI
     
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
@@ -28,14 +29,34 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: Timer!
 
+    
+    // Confugured Button Type with tag
     enum ButtonType: Int {case slow = 0, fast , chipmunk, vader, echo, reverb}
     
+    // Playing Back Audio by this Function
+    
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("Play Sound Button Pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
+   
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("Stop Audio Button Pressed")
+        
     }
     
     
